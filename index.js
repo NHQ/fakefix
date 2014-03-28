5d382ed9c9595126caa915bba94537ea350d489c
FX Mobile 16 <fxmobile16@FX-Mobile-16.local>
2014-03-27 23:35:24 -0700

    fixed fakefix

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ module.exports = function(element){
   var el = element
   var l = parseFloat(getCSS(el, 'left'))
   var t = parseFloat(getCSS(el, 'top'))
+  var r = parseFloat(getCSS(el, 'right'))
+  var b = parseFloat(getCSS(el, 'bottom'))
+  if(isNaN(l)){
+    l = window.innerWidth - parseFloat(getCSS(el, 'right')) - parseFloat(getCSS(el, 'width'))
+    if(isNaN(l)) l = 0
+  }
+  if(isNaN(t)){
+    t = window.innerHeight - parseFloat(getCSS(el, 'bottom')) - parseFloat(getCSS(el, 'height'))
+    if(isNaN(t)) t = 0
+  }
   var t0 = false
   window.addEventListener('scroll', function(evt){
     if(t0) clearTimeout(t0)
